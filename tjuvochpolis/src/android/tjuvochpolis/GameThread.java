257704cb7719c82a242687609d1c919b1dea0ddd
marjo424@student.liu.se <marjo424@student.liu.se@66fb2d68-6d78-a46d-c522-9a7bf0b80fdc>
2011-03-29 12:34:34 +0000

    GameView med run-funktion som anropar doDraw() hos GameState.

--- tjuvochpolis/src/android/tjuvochpolis/GameThread.java
+++ tjuvochpolis/src/android/tjuvochpolis/GameThread.java
@@ -1,5 +1,8 @@
 package android.tjuvochpolis;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Handler;
 import android.view.SurfaceHolder;
 
@@ -14,25 +17,74 @@ public class GameThread extends Thread
 	
 	private Boolean mRun;
 	
-	// Eventuellt en background om thread själv ska rita bakgrunden
+	//private GameState mGameState;
+
+    private int mCanvasHeight = 1;
+
+    private int mCanvasWidth = 1;
 	
-	public GameThread(SurfaceHolder surfaceHolder, Context context, Handler handler)
+	public GameThread(SurfaceHolder surfaceHolder, Context context)
 	{
 		// Grid, Gameobjects, etc.
+		mSurfaceHolder = surfaceHolder;
+		mContext = context;
+		
+		//mGameState = new GameState();
 	}
+	
+	/**
+     * Used to signal the thread whether it should be running or not.
+     * Passing true allows the thread to run; passing false will shut it
+     * down if it's already running. Calling start() after this was most
+     * recently called with false will result in an immediate shutdown.
+     *
+     * @param b true to run, false to shut down
+     */
+    public void setRunning(boolean b) {
+        mRun = b;
+    }
 	
 	@Override
     public void run() {
         while (mRun) {
-        	//do something
+        	Canvas c = null;
+            try {
+                c = mSurfaceHolder.lockCanvas(null);
+                synchronized (mSurfaceHolder) {
+                    drawScreen(c);
+                }
+            } finally {
+                // do this in a finally so that if an exception is thrown
+                // during the above, we don't leave the Surface in an
+                // inconsistent state
+                if (c != null) {
+                    mSurfaceHolder.unlockCanvasAndPost(c);
+                }
+            }
+
         }
 	}
 	
+	/* Callback invoked when the surface dimensions change. */
+    public void setSurfaceSize(int width, int height) {
+        // synchronized to make sure these all change atomically
+        synchronized (mSurfaceHolder) {
+            mCanvasWidth = width;
+            mCanvasHeight = height;
+        }
+    }
 
 	
-	public void drawScreen()
+	public void drawScreen(Canvas canvas)
 	{
+		//ta bort när gamestate doDraw är aktiverat
+		int color = -16711681;
+		canvas.drawColor(color);
+		//------------------------
 		
+		//mGameState.doDraw(canvas);
 	}
+	
+
 	
 }
--- tjuvochpolis/src/android/tjuvochpolis/game.java
+++ tjuvochpolis/src/android/tjuvochpolis/game.java
@@ -9,5 +9,22 @@ public class game extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        // avkommentera när saved states blir aktuellt
+        /*// get handles to the GameView from XML, and its GameThread
+        mGameView = (GameView) findViewById(R.id.tjuvochpolis);
+        mGameThread = mGameView.getThread();
+
+        if (savedInstanceState == null) {
+            // we were just launched: set up a new game
+            mGameThread.setState(GameThread.STATE_READY);
+            Log.w(this.getClass().getName(), "SIS is null");
+        } else {
+            // we are being restored: resume a previous game
+            mGamehread.restoreState(savedInstanceState);
+            Log.w(this.getClass().getName(), "SIS is nonnull");
+        }
+        */
     }
 }
