18837fa106d1754663e20595dd588a34e5647c47
albca711@student.liu.se <albca711@student.liu.se@66fb2d68-6d78-a46d-c522-9a7bf0b80fdc>
2011-03-29 07:58:49 +0000

    Grid med GridNode och grannar

---
+++ trunk/tjuvochpolis/src/android/tjuvochpolis/Grid.java
@@ -0,0 +1,45 @@
+package android.tjuvochpolis;
+
+public class Grid {
+
+	int height = 18;
+	int width = 21;
+	GridNode gridArray[][] = new GridNode[width][height];
+
+	public Grid() {
+		// Puts GridNodes in array
+		for (int column = 0; column < width; column++) {
+			for (int row = 0; row < height; row++) {
+				gridArray[column][row] = new GridNode();
+			}
+		}
+
+		// sets the neighbors
+		for (int column = 0; column < width; column++) {
+			for (int row = 0; row < height; row++) {
+
+				if (row == 0)
+					gridArray[column][row].setUpNode(null);
+				else
+					gridArray[column][row]
+							.setUpNode(gridArray[column][row - 1]);
+				if (row == height - 1)
+					gridArray[column][row].setDownNode(null);
+				else
+					gridArray[column][row]
+							.setDownNode(gridArray[column][row + 1]);
+				if (column == 0)
+					gridArray[column][row].setLeftNode(null);
+				else
+					gridArray[column][row]
+							.setLeftNode(gridArray[column - 1][row]);
+				if (column == width - 1)
+					gridArray[column][row].setRightNode(null);
+				else
+					gridArray[column][row]
+							.setRightNode(gridArray[column + 1][row]);
+
+			}
+		}
+	}
+}
