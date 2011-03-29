508a9b730773180c57633a50b0d189e007c47630
marjo424@student.liu.se <marjo424@student.liu.se@66fb2d68-6d78-a46d-c522-9a7bf0b80fdc>
2011-03-29 12:34:34 +0000

    GameView med run-funktion som anropar doDraw() hos GameState.

--- trunk/tjuvochpolis/src/android/tjuvochpolis/GameView.java
+++ trunk/tjuvochpolis/src/android/tjuvochpolis/GameView.java
@@ -20,22 +20,46 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 		
 		SurfaceHolder holder = getHolder();
         holder.addCallback(this);
+        
+        thread = new GameThread(holder, context);
+        
+        setFocusable(true);
 
 	}
 	
 	public void surfaceCreated(SurfaceHolder holder)
 	{
-		//gör det som lunarview
+		// start the thread here so that we don't busy-wait in run()
+        // waiting for the surface to be created
+        thread.setRunning(true);
+        thread.start();
 	}
 	
 	public void surfaceDestroyed(SurfaceHolder holder)
 	{
-		
+		// we have to tell thread to shut down & wait for it to finish, or else
+        // it might touch the Surface after we return and explode
+        boolean retry = true;
+        thread.setRunning(false);
+        while (retry) {
+            try {
+                thread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+            }
+        }
+
 	}
 	
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) 
 	{
-		 
+		thread.setSurfaceSize(width, height);
+
+	}
+	
+	public GameThread getThread()
+	{
+		return thread;
 	}
 	
 }
