fc701c25e95b1eba94c64ef9149ebcf8144cd8c1
marjo424@student.liu.se <marjo424@student.liu.se@66fb2d68-6d78-a46d-c522-9a7bf0b80fdc>
2011-04-14 13:40:44 +0000

    Touch på porträtt

--- trunk/tjuvochpolis/src/android/tjuvochpolis/CopTurnState.java
+++ trunk/tjuvochpolis/src/android/tjuvochpolis/CopTurnState.java
@@ -26,7 +26,9 @@ public class CopTurnState extends PlayOrderState {
     private Paint mPaintBgRight;
     private Rect mRectLeft;
     private Rect mRectRight;
-    private Rect mRectCop;
+    private Rect mRectCop1;
+    private Rect mRectCop2;
+    private Rect mRectCop3;
     private Bitmap mHudBottomImage;
 	private Bitmap mCopImage;
 	
@@ -37,7 +39,11 @@ public class CopTurnState extends PlayOrderState {
 		Resources res = (ps.getContext()).getResources();
 		
 		mHudBottomImage = BitmapFactory.decodeResource(res, R.drawable.hud_bottom);       
-        mCopImage = BitmapFactory.decodeResource(res, R.drawable.cop);		
+        mCopImage = BitmapFactory.decodeResource(res, R.drawable.cop);
+        
+        mRectCop1 = new Rect();
+		mRectCop2 = new Rect();
+		mRectCop3 = new Rect();
 	}
 	
 	public void handleState(int frame)
@@ -82,59 +88,66 @@ public class CopTurnState extends PlayOrderState {
         
 		c.drawBitmap(resizedBitmapLeft, null, mRectLeft, null);
 		
-		mRectCop = new Rect(0, canvasHeight - thickness, thickness, canvasHeight);
-		c.drawBitmap(mCopImage, null, mRectCop, null);
-		mRectCop = new Rect(thickness*2, canvasHeight - thickness, thickness*3, canvasHeight);
-		c.drawBitmap(mCopImage, null, mRectCop, null);
-		mRectCop = new Rect(thickness*4, canvasHeight - thickness, thickness*5, canvasHeight);
-		c.drawBitmap(mCopImage, null, mRectCop, null);
+		mRectCop1.set(0, canvasHeight - thickness, thickness, canvasHeight);
+		c.drawBitmap(mCopImage, null, mRectCop1, null);
+		mRectCop2.set(thickness*2, canvasHeight - thickness, thickness*3, canvasHeight);
+		c.drawBitmap(mCopImage, null, mRectCop2, null);
+		mRectCop3.set(thickness*4, canvasHeight - thickness, thickness*5, canvasHeight);
+		c.drawBitmap(mCopImage, null, mRectCop3, null);
 			
 	}
 	
 	public void doTouch(View v, MotionEvent event) 
 	{
-		//om x och y är giltiga destinationer
-		int row = ((int) event.getY() - mPlayState.getOffsetY())/Grid.GRID_SIZE;
-		int col = ((int) event.getX() - mPlayState.getOffsetX())/Grid.GRID_SIZE;
-		
-		GridNode clickedNode =	mGrid.getGridNode(row, col);
-		currentObject =	clickedNode.getGameObject();
-		// If cop is clicked -> draw highlights
-		
-	//	if(currentObject != null && currentObject.getClass() == CopObject.class)
-		//{
-			
-			
-		//}
-		
-		//kollar om alla poliser har gått
-		if(this.mGameObjects.get(mObjectIndex.COP1.getIndex()).getCurrentDiceValue() == 0 && this.mGameObjects.get(mObjectIndex.COP2.getIndex()).getCurrentDiceValue() == 0){
-			everythingHasMoved = true;
-		}
-	
-		
-		
-		if(currentObject == null && lastSelected != null && lastSelected.getClass() == CopObject.class && lastSelected.getCurrentDiceValue() != 0)
+		if(event.getY() > 48 && event.getY() < v.getHeight() - 48)
 		{
-			for(ArrayList<GridNode> paths : lastSelected.getPossiblePaths())
-			{
-				if(paths.get(paths.size() - 1).equals(mGrid.getGridNode(row, col)))
-				{ 	
-					//Log.i("SELECTED","" + getCurrentObjectSelected());
-					hasMoved = true;
-					lastSelected.setMovePath(paths);
-					lastSelected.isMoving = true;
-					lastSelected.setCurrentDiceValue(0);
-					setCurrentObjectSelected(lastSelected.getName());
-				}
-				
+			//om x och y är giltiga destinationer
+			int row = ((int) event.getY() - mPlayState.getOffsetY())/Grid.GRID_SIZE;
+			int col = ((int) event.getX() - mPlayState.getOffsetX())/Grid.GRID_SIZE;
 			
-				//tempIndex = " ";
+			GridNode clickedNode =	mGrid.getGridNode(row, col);
+			currentObject =	clickedNode.getGameObject();
+	
+			//kollar om alla poliser har gått
+			if(this.mGameObjects.get(mObjectIndex.COP1.getIndex()).getCurrentDiceValue() == 0 && this.mGameObjects.get(mObjectIndex.COP2.getIndex()).getCurrentDiceValue() == 0){
+				everythingHasMoved = true;
 			}
-		
+			
+			if(currentObject == null && lastSelected != null && lastSelected.getClass() == CopObject.class && lastSelected.getCurrentDiceValue() != 0)
+			{
+				for(ArrayList<GridNode> paths : lastSelected.getPossiblePaths())
+				{
+					if(paths.get(paths.size() - 1).equals(mGrid.getGridNode(row, col)))
+					{ 	
+						//Log.i("SELECTED","" + getCurrentObjectSelected());
+						hasMoved = true;
+						lastSelected.setMovePath(paths);
+						lastSelected.isMoving = true;
+						lastSelected.setCurrentDiceValue(0);
+						setCurrentObjectSelected(lastSelected.getName());
+					}
+					
+				
+					//tempIndex = " ";
+				}
+			
+			}
+			else{
+				lastSelected = currentObject;
+			}
 		}
-		else{
-			lastSelected = currentObject;
+		else // Fifflar på HUD:en
+		{
+			float x = event.getX();
+			float y = event.getY();
+			int thickness = mRectCop1.width();
+			
+			if(x > 0 && x < thickness && y > v.getHeight()-thickness && y < v.getHeight()) // Porträtt 1
+				Log.i("Cop", "1");
+			else if(x > thickness*2 && x < thickness*3 && y > v.getHeight()-thickness && y < v.getHeight()) // Porträtt 1
+				Log.i("Cop", "2");
+			else if(x > thickness*4 && x < thickness*5 && y > v.getHeight()-thickness && y < v.getHeight()) // Porträtt 1
+				Log.i("Cop", "3");
 		}
 		
 	}
