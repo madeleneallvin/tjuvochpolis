97141142c2362cfe5b91bffcc1770a2bd9433b61
albca711@student.liu.se <albca711@student.liu.se@66fb2d68-6d78-a46d-c522-9a7bf0b80fdc>
2011-05-03 08:17:06 +0000

    Städade upp i BankObject och NestObject

--- tjuvochpolis/src/android/tjuvochpolis/BankObject.java
+++ tjuvochpolis/src/android/tjuvochpolis/BankObject.java
@@ -9,24 +9,31 @@ public class BankObject extends GameStaticObject {
 		this.setObjectMoney(getBankMoney());
 	}
 
-	public int getBankMoney(){
-		
+	/*
+	 * Function that generates the amount of money for a bank
+	 * The closer to the center the more money the bank will get
+	 */
+	public int getBankMoney()
+	{
+		float baseMoney = 10000;
 		Random r = new Random();
-		
 		float rand = r.nextFloat()+1;
+		
+		// Map center
 		int mapCenterHeight = (int)Grid.getHeight()/2;
 		int mapCenterWidth = (int)Grid.getWidth()/2;
 		
+		// Bank position
 		int x = this.getParentNode().getNodeX();
 		int y = this.getParentNode().getNodeY();
 		
+		// Distance to center
 		float dist = Math.abs(mapCenterHeight - x) + Math.abs(mapCenterWidth - y);
 		
-		float baseMoney = 10000;
+		// Calculate the amount of money
+		int money = (int)(rand * baseMoney/dist);
 		
-		int temp = (int)(rand * baseMoney/dist);
-		
-		return temp;
+		return money;
 		
 	}
 }
--- tjuvochpolis/src/android/tjuvochpolis/NestObject.java
+++ tjuvochpolis/src/android/tjuvochpolis/NestObject.java
@@ -1,6 +1,5 @@
 package android.tjuvochpolis;
 
-
 public class NestObject extends GameStaticObject {
 
 	public NestObject(String name, GridNode parentNode) {
@@ -8,12 +7,20 @@ public class NestObject extends GameStaticObject {
 		this.setObjectMoney(0);
 	}
 
+	/*
+	 * Function to store away money in the nest
+	 */
 	public void storeMoney(int money){
 		this.setObjectMoney(this.getObjectMoney() + money);
 	}
 	
+	/*
+	 * Function to take all the money from the nest
+	 */
 	public int takeMoney(){
-		return this.getObjectMoney();
+		int money = this.getObjectMoney();
+		this.setObjectMoney(0);
+		return money;
 	}
 	
 }
