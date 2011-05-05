260bf43f6b1c877eac6b3f23a529324d5978650f
gusbl659@student.liu.se <gusbl659@student.liu.se@66fb2d68-6d78-a46d-c522-9a7bf0b80fdc>
2011-05-05 07:52:40 +0000

    har lagt till animationer

--- tjuvochpolis/src/android/tjuvochpolis/Bitmaps.java
+++ tjuvochpolis/src/android/tjuvochpolis/Bitmaps.java
@@ -1,9 +1,15 @@
 package android.tjuvochpolis;
 
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Movie;
 
 public class Bitmaps {
 
@@ -14,12 +20,15 @@ public class Bitmaps {
 	private static Bitmap mCopImage;
 	private static Bitmap mThiefImage;
 	private static Bitmap mBackgroundImage;
-	
+	private static Movie movieThiefLeft,movieThiefRight,movieThiefUp,movieThiefDown;
+	private static Movie movieCopLeft,movieCopRight,movieCopUp,movieCopDown;
+	public static final int UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3;
+	private static ArrayList<Movie> thiefmovies = new ArrayList<Movie>(4);
 	private Bitmaps() {
 
 	}
 	
-	public static Bitmaps instance(Context context) {
+	public static Bitmaps instance(Context context){
 		if (bitmaps == null) {
 			bitmaps = new Bitmaps();
 			Resources res = context.getResources();
@@ -27,7 +36,17 @@ public class Bitmaps {
 			setHudBottomImageThieves(BitmapFactory.decodeResource(res, R.drawable.htc_desire_tjuv_small));
 			setHudTopImage(BitmapFactory.decodeResource(res, R.drawable.htc_top_bakgrund_256));
 			setCopImage(BitmapFactory.decodeResource(res, R.drawable.small_police));
-			setThiefImage(BitmapFactory.decodeResource(res, R.drawable.small_thief));
+			setThiefImage(BitmapFactory.decodeResource(res, R.drawable.buse1_standing));
+			
+			//Lägg alla movies i en indexerbar array.
+			movieThiefRight=Movie.decodeStream(context.getResources().openRawResource(R.drawable.buse1_walk_right_animate));
+			movieThiefLeft=Movie.decodeStream(context.getResources().openRawResource(R.drawable.buse1_walk_left_animate));
+			movieThiefUp=Movie.decodeStream(context.getResources().openRawResource(R.drawable.buse1_walk_up_animate));
+			movieThiefDown=Movie.decodeStream(context.getResources().openRawResource(R.drawable.buse1_walk_down_animate));
+			thiefmovies.add(UP, movieThiefUp);
+			thiefmovies.add(RIGHT, movieThiefRight);
+			thiefmovies.add(DOWN, movieThiefDown);
+			thiefmovies.add(LEFT, movieThiefLeft);
 			setBackgroundImage(BitmapFactory.decodeResource(res, R.drawable.map_small));       
 		}
 		return bitmaps;
@@ -79,5 +98,14 @@ public class Bitmaps {
 
 	public Bitmap getBackgroundImage() {
 		return mBackgroundImage;
+	}
+
+
+
+	
+	// ANIMATION ARRAYS
+	
+	public Movie getThiefmovies(int index) {
+		return thiefmovies.get(index);
 	}
 }
