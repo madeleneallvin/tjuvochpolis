c2ff5eca4bb86c6c35b3ba290876edb9712dc17a
gusbl659@student.liu.se <gusbl659@student.liu.se@66fb2d68-6d78-a46d-c522-9a7bf0b80fdc>
2011-05-05 07:52:40 +0000

    har lagt till animationer

--- trunk/tjuvochpolis/src/android/tjuvochpolis/ThiefObject.java
+++ trunk/tjuvochpolis/src/android/tjuvochpolis/ThiefObject.java
@@ -14,30 +14,48 @@ public class ThiefObject extends GameObject{
 	private int pocketMoney;
 	private Rect rectThief;
 	private Bitmap thiefIm;
+	private long moviestart;
 	
 	public ThiefObject(String name, GridNode parentNode) {
 		super(name, parentNode);
-
+		this.isMoving = false;
 		this.setDrawXPos(this.getParentNode().getPixelX());
 		this.setDrawYPos(this.getParentNode().getPixelY());
 	}
 
 	@Override
 	public void doDraw(Canvas canvas, int offsetX, int offsetY, Context context) {
+		
 		Paint paint = new Paint();
 		paint.setColor(Color.RED); //bara tillfälligt
 		//canvas.drawCircle(this.getDrawXPos()+Grid.GRID_SIZE/2 + offsetX, this.getDrawYPos()+Grid.GRID_SIZE/2 + offsetY, Grid.GRID_SIZE/2, paint);
 		
-		int left = (int) this.getDrawXPos() + offsetX + 7;
-		int right = left + 33;
-		int top = (int) this.getDrawYPos() + offsetY;
-		int bottom = (int) top + 48;
+		int xPos = (int) this.getDrawXPos() + offsetX ;
+		int right = xPos + 48;
+		int yPos = (int) this.getDrawYPos() + offsetY-26;
+		int bottom = (int) yPos + 74;
 		
-		Resources res = context.getResources();
-		thiefIm = Bitmaps.instance(context).getThiefImage();
-		rectThief = new Rect(left, top, right, bottom);
-		canvas.drawBitmap(thiefIm, null, rectThief, null);
 		
+		//lite "kod" som sätter frameraten rätt.
+		long now=android.os.SystemClock.uptimeMillis();
+		if (moviestart == 0) { // first time
+			moviestart = now;
+		}
+		if(this.isMoving == true){
+			
+			int direction = Bitmaps.DOWN; //välj denna beroende på vilket håll nästa nod är åt
+			
+			int relTime = (int)((now - moviestart) % Bitmaps.instance(context).getThiefmovies(direction).duration()) ;
+			Bitmaps.instance(context).getThiefmovies(direction).setTime(relTime);
+			Bitmaps.instance(context).getThiefmovies(direction).draw(canvas,xPos, yPos, paint);
+			//this.invalidate();
+		}
+		else{
+			Resources res = context.getResources();
+			thiefIm = Bitmaps.instance(context).getThiefImage();
+			rectThief = new Rect(xPos, yPos, right, bottom);
+			canvas.drawBitmap(thiefIm, null, rectThief, null);
+		}
 	}
 
 	@Override
