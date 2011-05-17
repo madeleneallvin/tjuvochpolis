960adea7029ead72f5ce717a82ec2412856a7f2a
gusbl659@student.liu.se <gusbl659@student.liu.se@66fb2d68-6d78-a46d-c522-9a7bf0b80fdc>
2011-05-17 08:24:28 +0000

    fixat så pjäser inte låses in om de blir blockade

--- trunk/tjuvochpolis/src/android/tjuvochpolis/ThiefMoveState.java
+++ trunk/tjuvochpolis/src/android/tjuvochpolis/ThiefMoveState.java
@@ -3,6 +3,7 @@ package android.tjuvochpolis;
 import java.util.ArrayList;
 
 import android.tjuvochpolis.PlayState.mObjectIndex;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -20,23 +21,57 @@ public class ThiefMoveState extends PlayOrderState {
 	}
 
 	public PlayOrderState getNextState() {	
+		
+		GridNode currentNode = mGameObjects.get(mObjectIndex.valueOf(getCurrentObjectSelected()).getIndex()).getParentNode();
+		
 		if(mGameObjects.get(mObjectIndex.valueOf(getCurrentObjectSelected()).getIndex()).isMoving) {
+			Log.i("thiefMoveState", "return to thiefMoveState");
 			return this;
 		}
-		else {
-			// Gets the current node
-			GridNode currentNode = mGameObjects.get(mObjectIndex.valueOf(getCurrentObjectSelected()).getIndex()).getParentNode();
+	//else {
+			ThiefObject currentThief = ((ThiefObject)mGameObjects.get(mObjectIndex.valueOf(getCurrentObjectSelected()).getIndex()));
+			/*	
+			if(currentThief.isCaught()){
+				
+				int thiefCol, thiefRow;
 			
-			// Depending of the type the event state is activated
+				if(currentThief.name.equalsIgnoreCase("thief1")){
+					thiefCol = 9;
+					thiefRow = 8;
+				}else if(currentThief.name.equalsIgnoreCase("thief2")){
+					thiefCol = 8;
+					thiefRow = 9;
+				}else{
+					thiefCol = 9;
+					thiefRow = 9;
+				}
+				
+				
+				currentThief.transportToJail(thiefRow,thiefCol, mGrid);
+				currentThief.setObjectMoney(0);
+				int wait = Dice.getDice().rollDice();
+				currentThief.setWaitingLeft(wait);
+								
+				//reset, låt detta stå efter alla ställen vi försöker accessa aktuell tjuv och polis
+				currentThief.setCaught(false);
+				mPlayState.mPreviousState = mPlayState.getThiefTurnState();
+				
+			//	return mPlayState.getThiefTurnState();
+			}
+			*/
+			
 			if(currentNode.getType() == GridNode.THIEF_NEST || currentNode.getType() == GridNode.BANK)
 			{
 				mPlayState.mPreviousState = mPlayState.getThiefTurnState();
+				Log.i("thiefMoveState", "return to getEventState");
 				return mPlayState.getEventState();
 			}
 			
+			Log.i("thiefMoveState", "return to thiefTurnState");
 			return mPlayState.getThiefTurnState();
-		}
+	//	}	
 	}
+
 
 	@Override
 	public void doTouch(View v, MotionEvent event) {
